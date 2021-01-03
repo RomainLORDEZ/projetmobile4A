508196eec3f83862b31e2094268406d7b288eb42
RomainLORDEZ <lordez@et.esiea.fr>
2021-01-03 22:27:41 +0100

    piga

--- app/src/main/java/com/esiea/projet4a/data/Model/Movy.kt
+++ app/src/main/java/com/esiea/projet4a/data/Model/Movy.kt
@@ -3,10 +3,10 @@ package com.esiea.projet4a.data.Model
 data class Movy (
     val id:Int = 0,
    val title:String? = null,
-   //val runtime:String? = null,
-   //val genres:List<String>?=null,
-   //val director:String? = null,
-   //val actors:String? = null,
-   //val plot:String? = null,
+   val runtime:String? = null,
+   val genres:List<String>?=null,
+   val director:String? = null,
+   val actors:String? = null,
+   val plot:String? = null,
     val posterUrl:String? = null
 )
